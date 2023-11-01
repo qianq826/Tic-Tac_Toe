adb02488260898fed255e8b7c144d73d0d6403bc
Sebastian Qian <148395429+qianq826@users.noreply.github.com>
2023-11-01 01:31:05 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,9 +1,25 @@
 from logic import make_empty_board
-from logic import show_board
-from logic import input_move
-from logic import change_turn
+from logic import other_player
 from logic import get_winner
 
+def show_board(board):
+	for row in board:
+		print(row)
+
+
+def input_move(board):
+	try:
+		r = int(input("input row index: "))
+		c = int(input("input col index: "))
+	except:
+		raise ValueError('input invalid, not int')
+	else:
+		if r < 0 or r > 2 or c < 0 or c > 2:
+			raise ValueError('input invalid, should >=0 and <=2')
+		if board[r][c] != '':
+			raise ValueError('input invalid, index already input')
+		return r, c
+        
 
 if __name__ == '__main__':
   board = make_empty_board()
@@ -17,4 +33,4 @@ if __name__ == '__main__':
      board[r][c] = turn
      show_board(board)
      winner = get_winner(board)
-     turn = change_turn(turn)
+     turn = other_player(turn)
