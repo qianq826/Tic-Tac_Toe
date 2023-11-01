9ea6da3c012d136abff4bb85908da6ce9c344313
Sebastian Qian <148395429+qianq826@users.noreply.github.com>
2023-11-01 01:51:47 -0400

    Update logic.py

--- logic.py
+++ logic.py
@@ -18,18 +18,23 @@ def get_winner(input_board):
         if input_board[i][0] is not None and input_board[i][0] == input_board[i][1] == input_board[i][2]:
             print(input_board[i][0], 'Won')
             return input_board[i][0]
-	for i in range(0, 3):
-		if input_board[0][i] != '' and input_board[0][i] == input_board[1][i] == input_board[2][i]:
-			print(input_board[0][i], ' Won')
-			return input_board[0][i]
-	if input_board[1][1] != '' and (
-			input_board[0][0] == input_board[1][1] == input_board[2][2] or input_board[2][0] == input_board[1][1] ==
-			input_board[0][2]):
-		print(input_board[1][1], ' Won')
-		return input_board[1][1]
-	for row in input_board:
-		for col in row:
-			if col == '':
-				return ''
-	print('Draw')
-	return 'Draw'
+
+    for i in range(0, 3):
+        if input_board[0][i] is not None and input_board[0][i] == input_board[1][i] == input_board[2][i]:
+            print(input_board[0][i], 'Won')
+            return input_board[0][i]
+
+    if input_board[1][1] is not None:
+        if input_board[0][0] == input_board[1][1] == input_board[2][2] or \
+                input_board[2][0] == input_board[1][1] == input_board[0][2]:
+            print(input_board[1][1], 'Won')
+            return input_board[1][1]
+
+    for row in input_board:
+        for cell in row:
+            if cell is None:  # Checking for None as an empty cell
+                return None
+
+    print('Draw')
+    return 'Draw'
+
