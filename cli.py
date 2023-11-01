3da381b948bb5b289cbc980072e0bb1721181673
Sebastian Qian <148395429+qianq826@users.noreply.github.com>
2023-11-01 01:43:40 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -8,18 +8,17 @@ def show_board(board):
 
 
 def input_move(board):
-	try:
-		r = int(input("input row index: "))
-		c = int(input("input col index: "))
-	except:
-		raise ValueError('input invalid, not int')
-	else:
-		if r < 0 or r > 2 or c < 0 or c > 2:
-			raise ValueError('input invalid, should >=0 and <=2')
-		if board[r][c] != '':
-			raise ValueError('input invalid, index already input')
-		return r, c
-        
+    try:
+        r = int(input("input row index: "))
+        c = int(input("input col index: "))
+    except:
+        raise ValueError('input invalid, not int')
+    else:
+        if r < 0 or r > 2 or c < 0 or c > 2:
+            raise ValueError('input invalid, should >=0 and <=2')
+        if board[r][c] is not None:  # Corrected this condition
+            raise ValueError('input invalid, index already input')
+        return r, c
 
 if __name__ == '__main__':
   board = make_empty_board()
