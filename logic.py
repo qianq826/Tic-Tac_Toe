67229972189637f8bc1dc2d4133579b3ccebb076
Sebastian Qian <148395429+qianq826@users.noreply.github.com>
2023-11-01 01:43:43 -0400

    Update logic.py

--- logic.py
+++ logic.py
@@ -8,18 +8,16 @@ def make_empty_board():
 
 
 def other_player(player):
-	if now == 'X':
-              next = 'Y"
-	else:
-		new = 'X'
-	return new
-
+    if player == 'X':
+        return 'Y'
+    else:
+        return 'X'
 
 def get_winner(input_board):
-	for i in range(0, 3):
-		if input_board[i][0] != '' and input_board[i][0] == input_board[i][1] == input_board[i][2]:
-			print(input_board[i][0], ' Won')
-			return input_board[i][0]
+    for i in range(0, 3):
+        if input_board[i][0] is not None and input_board[i][0] == input_board[i][1] == input_board[i][2]:
+            print(input_board[i][0], 'Won')
+            return input_board[i][0]
 	for i in range(0, 3):
 		if input_board[0][i] != '' and input_board[0][i] == input_board[1][i] == input_board[2][i]:
 			print(input_board[0][i], ' Won')
